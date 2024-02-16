f6da27b1f4fc5f75f55b58a2fa8066eb21ee6e33
odmrs <omarcosviniciusdev@gmail.com>
2024-02-16 16:47:16 -0300

    feat: first lesson and first hello world using the get programming with go book

---
+++ 00-base/00-unit/01-lesson/main.go
@@ -0,0 +1,10 @@
+package main // declares the package of this code belong to
+
+import "fmt" // makes the fmt package avaliable for use
+
+func main() { // Declare the main function
+	fmt.Println("Hello Marcos!") // Send to output the hello world phrase	
+	fmt.Println("Hello Hello, Ѝउ!")
+	fmt.Println("Γεια σου κόσμε ")
+	fmt.Println("мир приветствий ")
+}
